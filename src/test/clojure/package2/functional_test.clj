847a4019304c9d061e2aa69d627ca471d8d4fec2
Mark Kelly <mkelly28@tcd.ie>
2015-06-01 16:57:34 +0100

    functional chapter 2
    * reduce

--- src/test/clojure/package2/functional_test.clj
+++ src/test/clojure/package2/functional_test.clj
@@ -5,7 +5,7 @@
 
 ;HOF - Higher Order Functions (Take functions as arguments or return a function as result)
 (deftest call-twice-test
-  (call-twice println 123) )
+  (call-twice println 123))
 
 (deftest max-test
   (is (= (max 5 6) 6)))
@@ -20,5 +20,31 @@
 
 (deftest map-double-vector-test
   (is (= (map * [1 2 3 4] [5 6 7 8]) '(5 12 21 32))))
+
+(comment similar to doing (max (max (max 0 -3) 10) 48) manually)
+(deftest reduce-test
+  (is (= (reduce max [0 -3 10 48]) 48)))
+
+(deftest reduce-add-test
+  (is (= (reduce + 50 [1 2 3 4]) 60)))
+
+(comment #() can reduce the anonymous function fn. See next example)
+(deftest reduce-collection-to-map-test
+  (is (= (reduce
+           (fn [m v]
+             (assoc m v (* v v)))
+           {} [1 2 3 4]) {4 16, 3 9, 2 4, 1 1}))
+  )
+
+(deftest reduce-collection-to-map-better-test
+  (is (= (reduce
+           #(assoc % %2 (* %2 %2))
+           {}
+           [1 2 3 4]) '{4 16, 3 9, 2 4, 1 1}))
+  )
+
+
+
+
 
 
