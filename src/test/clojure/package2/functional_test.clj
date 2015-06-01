ac6064cbd8cf52767a583c5a245ce63994d4c5b0
Mark Kelly <mkelly28@tcd.ie>
2015-06-01 17:38:44 +0100

    functional chapter 2
    * comp

--- src/test/clojure/package2/functional_test.clj
+++ src/test/clojure/package2/functional_test.clj
@@ -1,6 +1,7 @@
 (ns package2.functional-test
   (:require [clojure.test :refer :all]
             [chapter2.functional :refer :all]
+            [clojure.string :as str]
             ))
 
 ;HOF - Higher Order Functions (Take functions as arguments or return a function as result)
@@ -42,6 +43,47 @@
            {}
            [1 2 3 4]) '{4 16, 3 9, 2 4, 1 1}))
   )
+
+(deftest apply-test
+  (is (= (apply hash-map [:a 5 :b 6]) {:a 5, :b 6})))
+
+(def only-strings (partial filter string?))
+
+(deftest only-strings-test
+  (is (= (only-strings ["a" 5 "b" 6]) ["a" "b"])))
+
+(deftest filter-string-test
+  (is (= (#(filter string? %) ["a" 5 "b" 6]) ["a" "b"])))
+
+(deftest filter-string-2-test
+  (is (= (#(filter % ["a" 5 "b" 6]) string?) ["a" "b"])))
+
+(deftest filter-number
+  (is (= (#(filter % ["a" 5 "b" 6]) number?) [5 6])))
+
+(comment function literals force you to fully specify all of the arguments to
+         the functions it calls, whereas partial allows you to be ignorant of such details)
+
+(comment the same as
+         (defn negated-sum-str
+           [& numbers]
+           (str (- (apply + numbers))))
+
+         comp is composition)
+(def negated-sum-str (comp str - +))
+
+(deftest negated-sum-str-test
+  (is (= (negated-sum-str 10 12 3.4) "-25.4")))
+
+(def camel->keyword (comp keyword
+                          str/join
+                          (partial interpose \-)
+                          (partial map str/lower-case)
+                          #(str/split % #"(?<=[a-z])(?=[A-Z])")))
+
+(deftest camel->keyword->test->foo->hey
+  (is (= (camel->keyword "CamelCase") ':camel-case)))
+
 
 
 
